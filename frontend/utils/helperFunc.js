ed6b41d05526cbad166e797bf780fc47ae4bc9bb
Ayesha <68371207+akhalil95@users.noreply.github.com>
2023-02-18 18:28:04 -0700

    added map to safety page.
    Added API code to JS and styled it in CSS

--- frontend/utils/helperFunc.js
+++ frontend/utils/helperFunc.js
@@ -1,3 +1,22 @@
 const helperFuncThatDoesSthHelpful = () => {
-    console.log("i did sth helpful yay")
+  console.log("i did sth helpful yay");
+};
+
+//map for safety page
+function initMap() {
+  var calgary = { lat: 51.0447, lng: -114.0719 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: calgary,
+  });
+
+  var pins = [];
+  map.addListener("click", function (event) {
+    var pin = new google.maps.Marker({
+      position: event.latLng,
+      map: map,
+    });
+
+    pins.push(pin);
+  });
 }
